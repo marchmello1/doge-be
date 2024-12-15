ca50217709629ba18b073c307cd63d9e7d8b5a98
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-12-15 16:53:00 +0530

    Delete api/bot/__init__.py

--- api/bot/__init__.py
+++
@@ -1,9 +0,0 @@
-from .api.bot.methods import RelocationBot
-from .api.bot.models import ChatInput, ChatResponse, Message
-
-__all__ = [
-    "RelocationBot",
-    "ChatInput",
-    "ChatResponse",
-    "Message"
-]
