e0e0c6b20a4a9c97bc452dfa4e653b6e72173b2f
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-12-15 14:29:35 +0530

    Update models.py

--- api/compare/models.py
+++ api/compare/models.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional, Union
 
-
 class BaseItem(BaseModel):
     title: str
     description: str
@@ -9,36 +8,5 @@ class BaseItem(BaseModel):
     impact: str
     source_link: Optional[str] = None
 
-
-class LawItem(BaseItem):
-    pass
-
-class LawData(BaseModel):
-    law_difference_data: List[LawItem]
-
-class TaxItem(BaseItem):
-    pass
-
-class TaxData(BaseModel):
-    tax_difference_data: List[TaxItem]
-
-class EducationItem(BaseItem):
-    pass
-
-class EducationData(BaseModel):
-    education_difference_data: List[EducationItem]
-
-class EmergencyServiceItem(BaseItem):
-    pass
-
-class EmergencyServiceData(BaseModel):
-    emergency_service_difference_data: List[EmergencyServiceItem]
-
-class PropertyItem(BaseItem):
-    pass
-
-class PropertyData(BaseModel):
-    property_difference_data: List[PropertyItem]
-
 class Response(BaseModel):
-    data: Union[LawData, TaxData, EducationData, EmergencyServiceData, PropertyData]
+    data: List[BaseItem]
