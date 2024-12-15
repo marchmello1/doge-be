5b339d8944b536ac7026f7fc3e09b6825a59baef
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-12-15 14:17:50 +0530

    Create __init__.py

---
+++ api/bot/__init__.py
@@ -0,0 +1,9 @@
+from .api.bot.methods import RelocationBot
+from .api.bot.models import ChatInput, ChatResponse, Message
+
+__all__ = [
+    "RelocationBot",
+    "ChatInput",
+    "ChatResponse",
+    "Message"
+]
